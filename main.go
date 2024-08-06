5d07a4382c16222d08581e1b8dc5bb0b6ea38ef5
SamYouatt <sdyouatt@gmail.com>
2024-08-06 08:57:49 +0100

    super basic hello world with bubbletea

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+type model struct {
+	runText string
+}
+
+func initModel() model {
+	return model{
+		runText: "Hello world",
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return m.runText
+}
+
 func main() {
-    fmt.Println("Hello world")
-    os.Exit(1)
+	program := tea.NewProgram(initModel())
+	if _, err := program.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
 }
