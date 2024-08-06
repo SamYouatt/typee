ff3ebfbd35dc4ff97151149e349152bf4867dcb7
SamYouatt <sdyouatt@gmail.com>
2024-08-06 18:17:39 +0100

    impl: centre text in terminal and style it a bit

--- main.go
+++ main.go
@@ -5,9 +5,25 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+type Colours struct {
+	Bg       lipgloss.Color
+	Fg       lipgloss.Color
+	FgSubtle lipgloss.Color
+}
+
+var colours = Colours {
+	Bg:       lipgloss.Color("#E1E1E3"),
+	Fg:       lipgloss.Color("#313437"),
+	FgSubtle: lipgloss.Color("#AAAEB3"),
+}
+
 type model struct {
+	width  int
+	height int
+
 	runText string
 }
 
@@ -23,19 +39,28 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
 			return m, tea.Quit
 		}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
 	}
 
 	return m, nil
 }
 
 func (m model) View() string {
-	return m.runText
+	textStyle := lipgloss.NewStyle().
+		Width(m.width).
+		Height(m.height).
+		Align(lipgloss.Center, lipgloss.Center).
+		Foreground(colours.FgSubtle).
+		Background(colours.Bg)
+
+	return textStyle.Render(m.runText)
 }
 
 func main() {
