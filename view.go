70c2eba6d34173fa4530660cbd165cafd41501df
SamYouatt <sdyouatt@gmail.com>
2024-08-08 20:03:47 +0100

    refactor: rendering the test should live in view file

--- view.go
+++ view.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 
 	"github.com/charmbracelet/lipgloss"
 )
@@ -43,6 +44,64 @@ func finishedTestView(m Model) string {
 		Align(lipgloss.Center, lipgloss.Center)
 
 	return testCompleteStyle.Render("Test complete! 🎉")
+}
+
+func (test Test) renderTest(width int) string {
+	untypedStyle := lipgloss.NewStyle().
+		Foreground(colours.FgSubtle).
+		Background(colours.Bg)
+	correctlyTypedStyle := lipgloss.NewStyle().
+		Foreground(colours.Fg).
+		Background(colours.Bg)
+	previousErrorStyle := lipgloss.NewStyle().
+		Foreground(colours.Error).
+		Background(colours.Bg)
+	currentPendingStyle := lipgloss.NewStyle().
+		Foreground(colours.FgSubtle).
+		Background(colours.BgSubtle)
+	currentlyInvalidStyle := lipgloss.NewStyle().
+		Foreground(colours.Error).
+		Background(colours.BgSubtle)
+
+	testLetters := []string{}
+
+	for index, letter := range test.text {
+		// Current character is wrong
+		if test.currentIndex == index && test.currentlyInvalid {
+			testLetters = append(testLetters, currentlyInvalidStyle.Render(string(letter)))
+			continue
+		}
+
+		// Current character pending
+		if test.currentIndex == index {
+			testLetters = append(testLetters, currentPendingStyle.Render(string(letter)))
+			continue
+		}
+
+		// Previously entered incorrectly
+		if slices.Contains(test.errorIndices, index) {
+			testLetters = append(testLetters, previousErrorStyle.Render(string(letter)))
+			continue
+		}
+
+		// Previously entered correctly
+		if test.currentIndex > index {
+			testLetters = append(testLetters, correctlyTypedStyle.Render(string(letter)))
+			continue
+		}
+
+		// Upcoming letters
+		testLetters = append(testLetters, untypedStyle.Render(string(letter)))
+	}
+
+	testRendered := lipgloss.JoinHorizontal(lipgloss.Center, testLetters...)
+
+	viewStyle := lipgloss.NewStyle().
+		Width(width).
+		AlignHorizontal(lipgloss.Center).
+		Background(colours.Bg)
+
+	return viewStyle.Render(testRendered)
 }
 
 func testView(m Model) string {
