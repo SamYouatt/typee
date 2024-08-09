dfb70e4d991179f6bd804bcb3d87f37b93098474
SamYouatt <sdyouatt@gmail.com>
2024-08-09 16:16:28 +0100

    calculate wpm of completed test

--- completed-test.go
+++ completed-test.go
@@ -3,15 +3,18 @@ package main
 import "time"
 
 type CompletedTest struct {
-	numWords int
-    timeTaken time.Duration
+	numWords  int
+	timeTaken time.Duration
+	wpm       int
 }
 
-func (t *Test) CompleteTest() CompletedTest {
+func (t *Test) CompleteTest() *CompletedTest {
 	timeTaken := t.stopwatch.ElapsedTime()
+	wpm := CalculateWpm(t.completedWords, timeTaken)
 
-	return CompletedTest{
-		numWords: t.numWords,
-        timeTaken: timeTaken,
+	return &CompletedTest{
+		numWords:  t.numWords,
+		timeTaken: timeTaken,
+		wpm:       wpm,
 	}
 }
