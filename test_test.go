97df256dcdef97f6457a5178952f23640fcff4cb
SamYouatt <sdyouatt@gmail.com>
2024-08-18 15:25:38 +0100

    update test casings on refactored functions

--- test_test.go
+++ test_test.go
@@ -12,7 +12,7 @@ func TestStopwatch_OnlyStartsOnFirstInput(t *testing.T) {
 		t.Errorf("Expected stopwatch to be stopped until first input")
 	}
 
-	test.PlayCharacter('T')
+	test.playCharacter('T')
 
 	if !test.stopwatch.started {
 		t.Errorf("Expected stopwatch to be running after first input")
@@ -22,7 +22,7 @@ func TestStopwatch_OnlyStartsOnFirstInput(t *testing.T) {
 func TestPlayCharacter_WithCorrectCharacter(t *testing.T) {
 	test := NewTest("Test text")
 
-	test.PlayCharacter('T')
+	test.playCharacter('T')
 
 	if test.currentIndex != 1 {
 		t.Errorf("Expected correct character to advance index")
@@ -32,7 +32,7 @@ func TestPlayCharacter_WithCorrectCharacter(t *testing.T) {
 func TestPlayCharacter_WithIncorrectCharacter(t *testing.T) {
 	test := NewTest("Test text")
 
-	test.PlayCharacter('x')
+	test.playCharacter('x')
 
 	if test.currentIndex != 0 {
 		t.Errorf("Should not have advanced current index for incorrect letter")
@@ -53,12 +53,12 @@ func TestPlayCharacter_WithIncorrectCharacter(t *testing.T) {
 
 func TestPlaySpace_WhenCorrectAdvancesWordCount(t *testing.T) {
 	test := NewTest("Test text")
-	test.PlayCharacter('T')
-	test.PlayCharacter('e')
-	test.PlayCharacter('s')
-	test.PlayCharacter('t')
+	test.playCharacter('T')
+	test.playCharacter('e')
+	test.playCharacter('s')
+	test.playCharacter('t')
 
-	test.PlaySpace()
+	test.playSpace()
 
 	if test.currentIndex != 5 {
 		t.Errorf("Expected correct space to advance index")
@@ -71,12 +71,12 @@ func TestPlaySpace_WhenCorrectAdvancesWordCount(t *testing.T) {
 
 func TestPlaySpace_WhenCorrect_TracksRunningWpm(t *testing.T) {
 	test := NewTest("Test text")
-	test.PlayCharacter('T')
-	test.PlayCharacter('e')
-	test.PlayCharacter('s')
-	test.PlayCharacter('t')
+	test.playCharacter('T')
+	test.playCharacter('e')
+	test.playCharacter('s')
+	test.playCharacter('t')
 
-	test.PlaySpace()
+	test.playSpace()
 
 	if len(test.runningWpm) != 1 {
 		t.Errorf("Expected running wpm to be calculated when word was completed")
@@ -87,7 +87,7 @@ func TestPlaySpace_WhenIncorrect(t *testing.T) {
 	test := NewTest("Test text")
 	test.currentIndex = 3
 
-	test.PlaySpace()
+	test.playSpace()
 
 	if test.currentIndex != 3 {
 		t.Errorf("Should not have increased the current index or incorrect entry")
@@ -110,7 +110,7 @@ func TestPlayBackspace_WhenYetToTypeLetter(t *testing.T) {
 	test := NewTest("Test text")
 	test.currentIndex = 2
 
-	test.PlayBackspace()
+	test.playBackspace()
 
 	if test.currentIndex != 2 {
 		t.Errorf("Should not be able to go back when pending letter")
@@ -121,9 +121,9 @@ func TestPlayBackspace_WhenIncorrectLetterEntered(t *testing.T) {
 	test := NewTest("Test text")
 	test.currentIndex = 2
 
-	test.PlayCharacter('x')
+	test.playCharacter('x')
 
-	test.PlayBackspace()
+	test.playBackspace()
 
 	if test.currentIndex != 2 {
 		t.Errorf("Should not be able to go back when pending letter")
@@ -134,7 +134,7 @@ func TestPlayCharacter_OnLastCharacter_MarksTestAsComplete(t *testing.T) {
 	test := NewTest("Test")
 	test.currentIndex = 3
 
-	test.PlayCharacter('t')
+	test.playCharacter('t')
 
 	if !test.complete {
 		t.Errorf("Should have been marked as complete after last character correct")
@@ -145,7 +145,7 @@ func TestPlayCharacter_OnNonLastCharacter_ReturnsFalse(t *testing.T) {
 	test := NewTest("Test")
 	test.currentIndex = 2
 
-	if test.PlayCharacter('s') {
+	if test.playCharacter('s') {
 		t.Fatalf("Expected to return false when test not complete")
 	}
 }
@@ -154,7 +154,7 @@ func TestPlayCharacter_OnLastCharacter_ReturnsTrue(t *testing.T) {
 	test := NewTest("Test")
 	test.currentIndex = 3
 
-	if !test.PlayCharacter('t') {
+	if !test.playCharacter('t') {
 		t.Fatalf("Expected completed test to return true")
 	}
 }
