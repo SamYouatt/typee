4335e521b1079a756ccd7b588dd590eba8e03a78
SamYouatt <sdyouatt@gmail.com>
2024-08-18 16:46:09 +0100

    move udate into tui/update

--- main.go
+++ main.go
@@ -10,34 +10,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-			if m.state == Ready {
-				m.state = InTest
-				return m, nil
-			}
-		}
-
-		if m.test.PlayInput(msg.String()) {
-			m.result = m.test.CompleteTest()
-			m.test = nil
-			m.state = TestComplete
-		}
-
-		return m, nil
-
-	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
-	}
-
-	return m, nil
-}
 
 func main() {
 	program := tea.NewProgram(initModel(), tea.WithAltScreen())
